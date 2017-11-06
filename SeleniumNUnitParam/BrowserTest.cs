b44836ae45634a144b8406e6b2021f9da9bdc676
toolchaindemo <33333422+toolchaindemo@users.noreply.github.com>
2017-11-06 18:26:56 +1300

    Update BrowserTest.cs

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,36 +15,11 @@ namespace SeleniumNUnitParam
         [Test]
         public void GoogleTest()
         {
-            /*
             Driver.Navigate().GoToUrl("http://www.google.com");
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
             System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("btnG")).Click();
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true), "The text selenium doest exist");
-            */ 
-            // Declaring and initialising the HtmlUnitWebDriver
-            HtmlUnitDriver unitDriver = new HtmlUnitDriver();
-            
-            // open google.com webpage
-            unitDriver.get("http://www.google.com");
-
-            System.out.println("Title of the page is -> " + unitDriver.getTitle());
-
-            // find the search edit box on the google page
-            WebElement searchBox = unitDriver.findElement(By.name("q"));
-
-            // type in Selenium
-            searchBox.sendKeys("Selenium");
-
-            // find the search button
-            WebElement button = unitDriver.findElement(By.name("btnG"));
-
-            // Click the button
-            button.click();
-            Assert.That(unitDriver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                            "The text selenium doest not exist");
-            System.out.println("Title of the page is -> " + unitDriver.getTitle());
-
         }
 
         [Test]
