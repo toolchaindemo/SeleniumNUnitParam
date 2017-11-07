af5c3b2a7a7f7672139055b3c2fb5b5a4645b204
toolchaindemo <33333422+toolchaindemo@users.noreply.github.com>
2017-11-08 12:37:54 +1300

    Update BrowserTest.cs

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -14,7 +14,7 @@ namespace SeleniumNUnitParam
         [Test]
         public void ExecuteAutomationTest()
         {
-            Driver.Navigate().GoToUrl("http://google.com");
+            Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
             Driver.FindElement(By.Name("UserName")).SendKeys("admin");
             Driver.FindElement(By.Name("Password")).SendKeys("admin");
             Driver.FindElement(By.Name("Login")).Submit();
