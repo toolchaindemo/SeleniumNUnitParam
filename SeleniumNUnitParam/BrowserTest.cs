4279456a38d9367d075ff4d5e565c696365d1d05
toolchaindemo <33333422+toolchaindemo@users.noreply.github.com>
2017-11-08 11:54:31 +1300

    Update BrowserTest.cs

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -18,8 +18,6 @@ namespace SeleniumNUnitParam
             Driver.Navigate().GoToUrl("http://www.google.com");
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
             System.Threading.Thread.Sleep(5000);
-            //Driver.FindElement(By.Name("btnG")).Click();
-            // ignore
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true), "The text selenium doest exist");
         }
 
